11d6017666baff50a6b9a8ee924973d41dcf0108
Jan Kübe <jan.kybe.007@gmail.com>
2018-12-09 10:59:39 +0000

    Removed duplicated directorys

--- src/components/backend/dragons/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'start_turn/(?P<gameid>[-\w]+)', views.startTurn, name="startTurn"),
-    url(r'start_game', views.startGame, name="startGame")
-]
